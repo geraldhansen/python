2bc46b212230aa7c7a077db7d3ce2b59cd5aebfa
Gerald Hansen <gerald.hansen@posteo.de>
2018-07-03 15:58:16 +0200

    Add pyscript.py template

---
+++ templates/pyscript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A simple python script template.
+"""
+
+import sys
+
+
+def main(arguments):
+    '''
+    The main function
+    '''
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
